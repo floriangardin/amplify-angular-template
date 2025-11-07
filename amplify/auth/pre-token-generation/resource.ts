199917400abac89e14a829360444f103b5f473ce
floriangardin <fgardin.pro@gmail.com>
2025-11-07 11:55:33 +0100

    set all to secrets

--- amplify/auth/pre-token-generation/resource.ts
+++ amplify/auth/pre-token-generation/resource.ts
@@ -1,11 +1,11 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const preTokenGeneration = defineFunction({
   name: 'pre-token-generation',
   entry: './handler.ts',
   resourceGroupName: 'auth',
   environment: {
-    STRIPE_SECRET_KEY: process.env['STRIPE_SECRET_KEY'] as string,
-    VERIFY_SUBSCRIPTION: process.env['VERIFY_SUBSCRIPTION'] as string,
+    STRIPE_SECRET_KEY: secret('STRIPE_SECRET_KEY'),
+    VERIFY_SUBSCRIPTION: "true",
   }
 });
--- amplify/functions/cancel-subscription/resource.ts
+++ amplify/functions/cancel-subscription/resource.ts
@@ -1,10 +1,10 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const cancelSubscription = defineFunction({
   name: 'cancel-subscription',
   entry: './handler.ts',
   timeoutSeconds: 15,
   environment: {
-    STRIPE_SECRET_KEY: process.env['STRIPE_SECRET_KEY'] as string,
+    STRIPE_SECRET_KEY: secret('STRIPE_SECRET_KEY'),
   },
 });
